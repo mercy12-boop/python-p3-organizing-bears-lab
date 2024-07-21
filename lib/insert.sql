a06fee66aa6018ab5eca5b2e3d6b69a7ee95ef3e
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-07-21 11:45:37 +0300

    Completes Lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 15, 'M', 'brown', 'calm', 0);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (2, 'Rowdy', 12, 'M', 'black', 'rowdy', 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (3, 'Tabitha', 7, 'F', 'white', 'calm', 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (4, 'Sergeant Brown
+', 18, 'M', 'black', 'calm', 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (5, 'Melissa', 8, 'F', 'brown', 'happy', 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (6, 'Grinch', 6, 'M', 'green', 'annoyed', 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (7, 'Wendy', 2, 'M', 'black', 'curious', 1);
+INSERT INTO bears ( id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 20, 'M', 'brown', 'unknown', 0);
